7b2ccbbf7bda218cf52376679702cb3fc3a23f7e
cantelerai <vieira.raiany@escola.pr.gov.br>
2025-04-05 22:28:40 -0300

    xccx

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mostrarMensagem() {
-    alert("Que Deus continue te abençoando e enchendo sua vida de alegrias. Feliz aniversário!");
+    alert("Que Deus continue te abençoando e enchendo sua vida de alegrias,Você é um homem incrivel, esforçado e muito trabalhador, saiba que se inspiramos muito em você e amamos cada momento ao seu lado mas quando você não guarda as coisas não kkk. Muito obrigada por ser o melhor pai do mundo. . Feliz aniversário!");
   }
   
   
